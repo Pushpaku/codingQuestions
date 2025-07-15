154ebdb7509472dc81fe00ba7b6d666b2115b392
Pushpaku <39005585+Pushpaku@users.noreply.github.com>
2025-07-15 23:00:31 +0530

    Update SolutionString.java

--- SolutionString.java
+++ SolutionString.java
@@ -6,7 +6,7 @@ public class SolutionString {
 String str1 = "abc";
 String str2 = "abc";
 String s3 = str1.concat(str2);
-System.out.println(s3);
+System.out.println(s3);//o/p =abcabc and 2 object create concate create new object so obe is scp and one is heap memory 
 	}
 
 }
